80219d70ca45d92597ab53f4ca1196aca75d10ba
Eshop-project <eshop.uncproject@gmail.com>
2020-09-24 15:03:32 -0600

    Changed default web-redirect from signup page

--- eShopping/UserAccounts/views.py
+++ eShopping/UserAccounts/views.py
@@ -5,6 +5,6 @@ from .admin import UserCreationForm
 
 class SignUp(generic.CreateView):
     form_class = UserCreationForm
-    success_url = reverse_lazy('login')
+    success_url = reverse_lazy('store')
     # Try to force login and put user back in main page.
     template_name = 'signup.html'
