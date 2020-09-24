7207f3564851a0b425d9d8fc3237c8ae01ba61e2
Eshop-project <eshop.uncproject@gmail.com>
2020-09-23 23:40:05 -0600

    Added custom user model

---
+++ eShopping/eShop/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+def store(request):
+    contex = {}
+    return render(request, 'store.html', contex)
+
+def cart(request):
+    contex = {}
+    return render(request, 'cart.html', contex)
+
+def checkout(request):
+    contex = {}
+    return render(request, 'checkout.html', contex)
